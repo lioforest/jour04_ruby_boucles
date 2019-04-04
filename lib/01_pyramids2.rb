e998ff604c296b9f058df117e33cf7e29bdf2b40
lioforest <lioforest@free.fr>
2019-04-04 22:52:53 +0200

    avec stat

--- lib/01_pyramids2.rb
+++
@@ -1,7 +0,0 @@
-def half_pyramids  puts "Entrez combien d'etage tu veux?"
- print ">  "  
-
-
-        puts "Bonjour, merci d'indiquer le nombre d'étage que tu veux ?"
-        print "> "
-        number = gets.to_i
--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,51 +1,71 @@
-puts "Bienvenue dans notre super jeux de l'escalier"
 
-$nb_partie=1
+def partie 
 
-while $nb_partie<5 
-
-				$stair_level=0
-				$nb_tour=0
+		puts "Bienvenue dans notre super jeux de l'escalier"
 
 
-				def lancé_du_dé
-
-					
-				#	print "> Appui sur entrer pour lancer le dé"
-
-				#	x = gets
-
-					$n = rand (6)
-
-					if $n==1 then
-							$stair_level-=1
-							puts "Vous avez fait #{$n}. Vous descendez d'une marche, vous êtes toujours sur la marche #{$stair_level}"
-					elsif $n==2 then
-							puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
-					elsif $n==3 then
-							puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
-					elsif $n==4 then
-							puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
-
-					else
-							$stair_level+=1
-							puts "Vous avez fait #{$n}. Vous montez d'une marche, vous êtes à présent sur la marche #{$stair_level}"
-
-					end
-
-				end
-
-				while $stair_level<10
-
-					lancé_du_dé
-					$nb_tour+=1
+		
+						$stair_level=0
+						$nb_tour=0
 
 
-				end
+						def lancé_du_dé
+							
+						#	print "> Appui sur entrer pour lancer le dé"
+						#	x = gets
+							$n = rand (1..6)
+							if $n==1 then
+									$stair_level-=1
+									puts "Vous avez fait #{$n}. Vous descendez d'une marche, vous êtes toujours sur la marche #{$stair_level}"
+							elsif $n==2 then
+									puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
+							elsif $n==3 then
+									puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
+							elsif $n==4 then
+									puts "Vous avez fait #{$n}. Vous ne bougez pas, vous êtes toujours sur la marche #{$stair_level}"
 
-				puts "super vous avez gagné en #{$nb_tour} tour"
+							else
+									$stair_level+=1
+									puts "Vous avez fait #{$n}. Vous montez d'une marche, vous êtes à présent sur la marche #{$stair_level}"
 
-	$nb_partie+=1
+							end
+
+						end
+
+						while $stair_level<10
+
+							lancé_du_dé
+							$nb_tour+=1
+
+
+						end
+
+						puts "super vous avez gagné en #{$nb_tour} tour"
+
+
+end 
+
+
+def average_finish_time
+
+	$nb_partie=0
+	$stat_score = 0
+
+		while $nb_partie<5 
+
+			partie
+
+			$stat_score = $stat_score + $nb_tour
+
+			$nb_partie+=1
+			
+		end
 	
+	$final_average= $stat_score.to_f/$nb_partie
+
 end
+
+average_finish_time
 puts "vous avez fait #{$nb_partie} partie"
+puts "Vous avez atteint la 10ème en marche au bout de #{$final_average} en moyenne."
+
